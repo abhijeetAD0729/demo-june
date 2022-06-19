a67b5c27b212279d4422b074d7f3d1cc7c389983
ranjit <ranjit@DESKTOP-5QPPC2Q>
2022-06-19 16:33:40 +0530

    [Abhijeet]-helo this code is releted to simple java code

---
+++ src/sample/demo1.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hi abhijeet how are you");
+	}
+}
---
+++ src/sample/demo2.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class demo2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hi i am abhijeet patil");
+	}
+}
